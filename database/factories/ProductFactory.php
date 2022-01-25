5f349d0361ccfed61876bd0f941f06a3f6988485
harisfi <acc.cp.haris@gmail.com>
2022-01-25 09:58:41 +0700

    Add product model, factory, migration, and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(3, true),
+            'price' => $this->faker->numberBetween(1, 500000),
+            'picture' => $this->faker->image(),
+            'rating' => $this->faker->numberBetween(3, 5),
+            'qty' => $this->faker->numberBetween(1, 50)
+        ];
+    }
+}
